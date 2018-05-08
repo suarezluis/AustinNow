e6e4fa2a13090bd2feae86cd0b7014ccfcaa82db
suarezluis <suarezluis@gmail.com>
2018-05-08 14:41:43 -0500

    added icons

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,2 +1,9 @@
+populateIcon(searchIcon,"#AA")
+populateIcon(gMapsIcon,"#BA")
+populateIcon(trafficIcon,"#CA")
+populateIcon(eventbriteIcon,"#DA")
+populateIcon(moviesIcon,"#EA")
+
+
 displayDate("#CC");
 displayTime("#DC");
