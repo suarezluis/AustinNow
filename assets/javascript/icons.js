4cd3083f2012f73aa7938eb8682d04dca8fe74a2
suarezluis <suarezluis@gmail.com>
2018-05-10 05:00:19 -0500

    added tracking funcionality

--- assets/javascript/icons.js
+++ assets/javascript/icons.js
@@ -6,3 +6,5 @@ var searchIcon = '<img class="icon search" src="assets/images/icons/searchIcon.s
 var trafficIcon = '<img class="icon traffic" src="assets/images/icons/trafficIcon.png" alt="">';
 var aboutIcon = '<img class="icon about" src="assets/images/icons/aboutIcon.png" alt="">';
 var amazonIcon = '<img class="icon amazon" src="assets/images/icons/amazonIcon.png" alt="">';
+var spotifyIcon = '<img class="icon spotify" src="assets/images/icons/spotifyIcon.png" alt="">';
+var helpIcon = '<img class="icon help" src="assets/images/icons/helpIcon.png" alt="">';
