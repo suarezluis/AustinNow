0295492efaf99fc2e43522e18f2bdefc2bc72543
suarezluis <suarezluis@gmail.com>
2018-05-10 06:18:40 -0500

    fixed google maps icon

--- assets/javascript/icons.js
+++ assets/javascript/icons.js
@@ -1,5 +1,5 @@
 var eventbriteIcon = '<img class="icon eventbrite" src="assets/images/icons/eventbriteIcon.svg" alt="">';
-var gMapsIcon = '<img class="icon gMaps" src="assets/images/icons/gMapsIcon.svg" alt="">';
+var gMapsIcon = '<img class="icon gMaps" src="assets/images/icons/gMapsIcon.png" alt="">';
 var googleIcon = '<img class="icon google" src="assets/images/icons/googleIcon.svg" alt="">';
 var moviesIcon = '<img class="icon movies" src="assets/images/icons/moviesIcon.svg" alt="">';
 var searchIcon = '<img class="icon search" src="assets/images/icons/searchIcon.svg" alt="">';
