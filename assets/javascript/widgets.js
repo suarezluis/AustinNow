d211d09b0425478d2bfdca3d20ef02a21600fda0
Luis Suarez <suarezluis@gmail.com>
2018-07-23 02:52:02 -0500

    added contact info

--- assets/javascript/widgets.js
+++ assets/javascript/widgets.js
@@ -3,7 +3,7 @@ var googleSearch =
 var gMaps =
   '<form><input type="text" name="" id="googleInput" placeholder="Google Maps Search"><input id="googleButton"type="submit" value="Search"></form>';
 var about =
-  "<h5>About</h5><p class='about'>First and foremost, thanks for visiting Austin Now! This is our first project for the Coding Bootcamp at The University of Texas in Austin. Austin Now! is and always will be 100% FREE. If you want to help out with it's developement and hosting costs, go ahead and treat yourself and buy something from Amazon using our link or shortcut. We will get a small kick back.</p><h5>Terms and Conditions:</h5><p>By accesing Austin-Now.com you accept and authorize anonymous collection of data that will help us further develop this website.</p><h5>Programmed by:</h5><p>Luis Suarez, Raven Wills, Patrick Thacker and Alan Davila</p>";
+  "<h5>About</h5><p class='about'>First and foremost, thanks for visiting Austin Now! This is our first project for the Coding Bootcamp at The University of Texas in Austin. Austin Now! is and always will be 100% FREE. If you want to help out with it's developement and hosting costs, go ahead and treat yourself and buy something from Amazon using our link or shortcut. We will get a small kick back.</p><h5>Terms and Conditions:</h5><p>By accesing Austin-Now.com you accept and authorize anonymous collection of data that will help us further develop this website.</p><h5>Programmed by:</h5><p>Luis Suarez, contact me via <a href='http://suarezluis.com/contact.html'>suarezluis.com</a> </p>";
 var help =
   "<h5>Help</h5><h5>Shortcuts:</h5>[A]mazon, [F]acebook, [G]oogle, [I]nstagram, [L]inkedIn, G[m]ail, [N]etflix, [R]eddit, [S]potify, [T]witter, [W]ikipedia, [Y]outube";
 var weatherDetails = [
